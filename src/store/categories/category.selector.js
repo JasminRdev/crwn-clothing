3f6ba1980d18e91eb8f35553160d154dc73c0bda
Jasmin <eyesthestic@gmail.com>
2023-07-01 17:32:23 +0200

    cart selector&reselect prevent rerender same data

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,29 @@
-export const selectCategoriesMap = (state) => 
-state.categoriesMap.categories
-    .reduce((acc, category) => {
-      const {title, items} = category
-      acc[title.toLowerCase()] = items;
-      return acc;
-    }, {})
+import {createSelector} from 'reselect'
+
+const selectCategoryReducer = (state) => {
+    console.log("selector 1", state)
+    return state.categoriesMap;
+}
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => {
+        console.log("selector 2", categoriesSlice)
+        return categoriesSlice.categories;
+    }
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log("selector 3")
+        return categories.reduce((acc, category) => {
+            const {title, items} = category
+            acc[title.toLowerCase()] = items;
+            return acc;
+          }, {})
+    }
+)
+
+
+
